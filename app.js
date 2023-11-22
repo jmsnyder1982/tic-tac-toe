d4d158d4bb6d4ca1e6b4e763818175afdd10f8d6
jmsnyder1982 <58143459+jmsnyder1982@users.noreply.github.com>
2023-11-22 18:49:59 -0500

    Finished tic tac toe game

---
+++ app.js
@@ -0,0 +1,79 @@
+import View from "./view.js";
+import Store from "./store.js";
+
+const players = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "turquoise",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "yellow",
+  },
+];
+function init() {
+  const view = new View();
+  const store = new Store("live-t3-storage-key", players);
+
+  function initView() {
+    view.closeAll();
+    view.clearMoves();
+    view.setTurnIndicator(store.game.currentPlayer);
+    view.updateScoreboard(
+      store.stats.playerWithStats[0].wins,
+      store.stats.playerWithStats[1].wins,
+      store.stats.ties
+    );
+    view.initializeMoves(store.game.moves);
+  }
+
+  window.addEventListener("storage", () => {
+    console.log("State changed from another tab");
+    initView();
+  });
+  initView();
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+    initView();
+  });
+
+  view.bindNewROundEvent((event) => {
+    store.newRound();
+    initView();
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    // Check if there is an existing move
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+
+    if (existingMove) {
+      return;
+    }
+
+    // Place an icon of the current player in a square
+    view.handlePlayerMove(square, store.game.currentPlayer);
+
+    // Advance to the next state by pushing a move to the moves array
+    store.playerMove(+square.id);
+
+    if (store.game.status.isComplete) {
+      view.openModal(
+        store.game.status.winner
+          ? `${store.game.status.winner.name} wins!`
+          : "Tie!"
+      );
+    }
+    // return;
+
+    // Set the next player's turn indicator
+    view.setTurnIndicator(store.game.currentPlayer);
+  });
+}
+window.addEventListener("load", init);
